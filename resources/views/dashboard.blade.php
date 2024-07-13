12c7b8254bcb7ce26db3218ec2d5a9dff4a122c2
mmashiqurrahman <m.m.ashiqur.rahman@gmail.com>
2024-07-13 11:38:51 +0600

    July 13th First Commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,6 +5,7 @@
 <div class="container">
   <h3>This is your Dashboard, <span class="text-danger">{{ $user->name }}</span></h3>
   <h3 class="bg-warning">Your email: {{ $user->email }}</h3>
+  <h4>Dashboard contains all of your News Feeds.</h4>
 </div>
 
 @endsection
