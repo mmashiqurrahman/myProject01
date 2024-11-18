32a938ec875ccd26bbfe9c23a4304d1b564eb327
ashiq <m.m.ashiqur.rahman@gmail.com>
2024-11-18 16:58:32 +0600

    18th_November_commit_01

---
+++ app/Models/UserPosts.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserPosts extends Model
+{
+    use HasFactory;
+
+    protected $table = "user_posts";
+
+    protected $fillable = [
+        'title',
+        'post',
+        'user_id'
+    ];
+}
