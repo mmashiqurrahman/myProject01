4f9a972e2357980ccf2289b538ee54de4c317bfa
ashiq <m.m.ashiqur.rahman@gmail.com>
2024-11-29 19:50:26 +0600

    1st commit, 29th November, 2024

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,36 +5,32 @@
   <h3>Sign Up</h3>
   <br>
 
-  @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-  @endif
-
   <form method="POST" action="/register">
     @csrf
     <!-- Name input -->
     <div class="form-outline mb-4">
-      <input type="text" id="form2Example0" class="form-control" name="name" />
+      <input type="text" id="form2Example0" class="form-control" name="name" value="{{ old('name') }}" />
       <label class="form-label" for="form2Example0">Name</label>
+      @error('name')
+        <div class="alert alert-danger">{{ $message }}</div>
+      @enderror
     </div>
 
     <div class="form-outline mb-4">
       <select class="form-control" name="role_id" id="role_id">
         @foreach($roles as $role)
-          <option value="{{ $role->id }}">{{ $role->name }}</option>
+          <option value="{{ $role->id }}" @if (old('role_id') == $role->id) selected @endif>{{ $role->name }}</option>
         @endforeach
       </select>
       <label class="form-label" for="role_id">Role</label>
     </div>
 
     <div class="form-outline mb-4">
-      <input type="email" id="form2Example1" class="form-control" name="email" />
+      <input type="email" id="form2Example1" class="form-control" name="email" value="{{ old('email') }}" />
       <label class="form-label" for="form2Example1">Email address</label>
+      @error('email')
+        <div class="alert alert-danger">{{ $message }}</div>
+      @enderror
     </div>
 
     <!-- Password input -->
